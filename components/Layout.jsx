cfbded91e7ecfc455be1af80ebf689029cdf2f22
Hossin <94375287+qwe2qwee@users.noreply.github.com>
2023-11-20 17:00:14 +0300

    fix

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,22 +1,17 @@
 import React from "react";
-import Head from "next/head";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 
-const Layout = ({children}) => {
+const Layout = ({ children }) => {
   return (
     <div className='layout'>
-      <Head>
-        <title>Ecommerce Store</title>
-        <link rel="icon" href="../public/vercel.svg" />
-      </Head>
       <header>
         <Navbar />
       </header>
       <main className='main-container'>{children}</main>
       <footer>
         <Footer />
-       </footer>
+      </footer>
     </div>
   );
 };
--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "../styles/globals.css";
 import { Layout } from "../components";
+import Head from "next/head";
 import { StateContext } from "../context/StateContext";
 import { Toaster } from "react-hot-toast";
 
@@ -8,7 +9,11 @@ const MyApp = ({ Component, pageProps }) => {
   return (
     <StateContext>
       <Layout>
-        <Toaster/>
+        <Head>
+          <title>Ecommerce Store</title>
+          <link rel='icon' href='/favicon.ico' sizes='any' />
+        </Head>
+        <Toaster />
         <Component {...pageProps} />
       </Layout>
     </StateContext>
